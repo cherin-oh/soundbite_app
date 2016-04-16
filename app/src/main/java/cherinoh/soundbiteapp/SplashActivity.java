cdf703d2b723f99e1c2259aac5dfe117c7852a2b
cherin.oh <che-rin.oh@macaulay.cuny.edu>
2016-04-16 12:24:28 -0400

    Initial commit

---
+++ app/src/main/java/cherinoh/soundbiteapp/SplashActivity.java
@@ -0,0 +1,18 @@
+package cherinoh.soundbiteapp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by cherinoh on 4/16/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
