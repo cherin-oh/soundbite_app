f9fb26ab1b31642bd5e9861ed82ecef1ba2d75e8
cherin.oh <che-rin.oh@macaulay.cuny.edu>
2016-04-16 20:29:07 -0400

    edit

--- app/src/main/java/cherinoh/soundbiteapp/MainActivity.java
+++ app/src/main/java/cherinoh/soundbiteapp/MainActivity.java
@@ -1,52 +1,157 @@
 package cherinoh.soundbiteapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.media.MediaPlayer;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imgButton1, imgButton2, imgButton3, imgButton4, imgButton5, imgButton6;  //Trump, Cruz, Carson, Obama, Kasich, Huckabee
+    Button button;
+    private MediaPlayer mp1, mp2, mp3, mp4, mp5, mp6, MLG; //same order, airhorn
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        imgButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imgButton1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                stopPlaying1();
+                stopPlaying2();
+                stopPlaying3();
+                stopPlaying4();
+                stopPlaying5();
+                stopPlaying6();
+                mp2 = MediaPlayer.create(MainActivity.this, R.raw.trump);
+                mp2.start();
+            }
+        });
+        imgButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imgButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying1();
+                stopPlaying2();
+                stopPlaying3();
+                stopPlaying4();
+                stopPlaying5();
+                stopPlaying6();
+                mp2 = MediaPlayer.create(MainActivity.this, R.raw.cruz2);
+                mp2.start();
+            }
+        });
+        imgButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        imgButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying1();
+                stopPlaying2();
+                stopPlaying3();
+                stopPlaying4();
+                stopPlaying5();
+                stopPlaying6();
+                mp3 = MediaPlayer.create(MainActivity.this, R.raw.ben);
+                mp3.start();
+            }
+        });
+
+        imgButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        imgButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying1();
+                stopPlaying2();
+                stopPlaying3();
+                stopPlaying4();
+                stopPlaying5();
+                stopPlaying6();
+                mp4 = MediaPlayer.create(MainActivity.this, R.raw.obama);
+                mp4.start();
+            }
+        });
+        imgButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        imgButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying1();
+                stopPlaying2();
+                stopPlaying3();
+                stopPlaying4();
+                stopPlaying5();
+                stopPlaying6();
+                mp5 = MediaPlayer.create(MainActivity.this, R.raw.kasich);
+                mp5.start();
+            }
+        });
+        imgButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        imgButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying1();
+                stopPlaying2();
+                stopPlaying3();
+                stopPlaying4();
+                stopPlaying5();
+                stopPlaying6();
+                mp6 = MediaPlayer.create(MainActivity.this, R.raw.huckabee);
+                mp6.start();
+            }
+        });
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MLG = MediaPlayer.create(MainActivity.this, R.raw.mlg);
+                MLG.start();
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void stopPlaying1(){
+        if (mp1 != null) {
+            mp1.stop();
+            mp1.release();
+            mp1 = null;
         }
-
-        return super.onOptionsItemSelected(item);
+    }
+    private void stopPlaying2(){
+        if (mp2 != null) {
+            mp2.stop();
+            mp2.release();
+            mp2 = null;
+        }
+    }
+    private void stopPlaying3(){
+        if (mp3 != null) {
+            mp3.stop();
+            mp3.release();
+            mp3 = null;
+        }
+    }
+    private void stopPlaying4(){
+        if (mp4 != null) {
+            mp4.stop();
+            mp4.release();
+            mp4 = null;
+        }
+    }
+    private void stopPlaying5(){
+        if (mp5 != null) {
+            mp5.stop();
+            mp5.release();
+            mp5 = null;
+        }
+    }
+    private void stopPlaying6(){
+        if (mp6 != null) {
+            mp6.stop();
+            mp6.release();
+            mp6 = null;
+        }
     }
 }
--- app/src/main/java/cherinoh/soundbiteapp/SplashActivity.java
+++ app/src/main/java/cherinoh/soundbiteapp/SplashActivity.java
@@ -2,17 +2,29 @@ package cherinoh.soundbiteapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+
+
 /**
  * Created by cherinoh on 4/16/16.
  */
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+//
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
     }
 }
